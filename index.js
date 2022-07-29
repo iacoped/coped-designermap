1d5e18b51b5246b80d9e578e9daa4689906c75ef
leonzh2k <leonzh2k@gmail.com>
2022-07-29 14:15:09 -0700

    implement first working version of marker merging algorithm

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-(async () => {
-
+// Protip: https://stackoverflow.com/questions/23429203/weird-behavior-with-objects-console-log
+(() => {
+    'use strict';
     const model = {
         data: null,
-        selectedMarkerData: null
+        selectedMarkerData: null,
+        markerViewMode: "overlap"
     }
 
+    // perhaps I will combine mapManager and markerManager into mapView since markers are on the mapview
     const mapManager = {
         init() {
             this.map = L.map('map', {
                 preferCanvas: true,
             }
             ).setView([37.439974, -15.117188], 1);
-        
+            
+            this.map.on("zoom", () => {
+                console.log("view reset");
+                markerManager.renderMarkers();
+                // console.log(this.map.getZoom());
+            })
+
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
                 minZoom: 2,
@@ -19,12 +28,7 @@
                 attribution: '© OpenStreetMap'
             }).addTo(this.map);
 
-            this.map.on("zoom", () => {
-                // console.log("view reset");
-                // console.log(controller.getPeopleData())
-                markerManager.renderMarkers(controller.getPeopleData(), true);
-                // console.log(this.map.getZoom());
-            })
+            
 
             // L.marker([51.5, 50]).addTo(this.map)
             //     .bindPopup('<img src="DesignBlogImage-1536x1097.jpg" height="200" width="200">', {
@@ -87,134 +91,174 @@
     const markerManager = {
         markerDOMEles: [],
 
-        renderMarkers(uniqueCoords, destroyExistingMarkers) {
-            if (destroyExistingMarkers) {
+        renderMarkers() {
+            const data = controller.getPeopleData();
+            if (true) { // maybe can consider not re-rendering if nothing changes
                 for (let i = 0; i < this.markerDOMEles.length; i++) {
                     this.markerDOMEles[i].removeFrom(mapManager.map);
                 }
                 this.markerDOMEles = [];
             }
-            const uniqueCoordsKeys = Object.keys(uniqueCoords);
-
-            arrForCheckingIntersections = [];
-            for (let i = 0; i < uniqueCoordsKeys.length; i++) {
-                let popupContent = `${uniqueCoords[uniqueCoordsKeys[i]].people.length}`;
+            const renderMode = controller.getMarkerViewMode();
+            const uniqueCoordsKeys = Object.keys(data);
+            if (renderMode === "merge") {
+                let arrForCheckingIntersections = [];
+                // let mergedMarkers = [];
+                for (let i = 0; i < uniqueCoordsKeys.length; i++) {
+                    arrForCheckingIntersections.push({
+                        id: uniqueCoordsKeys[i],
+                        coords: mapManager.map.latLngToContainerPoint([data[uniqueCoordsKeys[i]].lat, data[uniqueCoordsKeys[i]].lng]),
+                        radius: data[uniqueCoordsKeys[i]].people.length + mapManager.map.getZoom(),
+                        people: data[uniqueCoordsKeys[i]].people,
+                        members: [],
+                        inGroup: false
+                    });
+                }
                 
-                // console.log(mapManager.map.latLngToContainerPoint([uniqueCoords[uniqueCoordsKeys[i]].lat, uniqueCoords[uniqueCoordsKeys[i]].lng]));
-                let marker = new L.circleMarker(
-                    [uniqueCoords[uniqueCoordsKeys[i]].lat, uniqueCoords[uniqueCoordsKeys[i]].lng],
-                    {
-                        color: "#FF5710",
-                        radius: uniqueCoords[uniqueCoordsKeys[i]].circleRadius + mapManager.map.getZoom(),
-                        stroke: false,
-                        fillOpacity: 0.5
-                    }
-                )
-                arrForCheckingIntersections.push({
-                    coords: mapManager.map.latLngToContainerPoint([uniqueCoords[uniqueCoordsKeys[i]].lat, uniqueCoords[uniqueCoordsKeys[i]].lng]),
-                    radius: marker.getRadius(),
-                    markerRef: marker
-                });
-                // marker.bindPopup(popupContent);
-
-                this.markerDOMEles.push(marker);
-
-                marker.addTo(mapManager.map);
-                // marker.on("add", () => {
-                //     console.log("added to map")
-                // })
-
-                marker.on("click", (e) => {
-                    // if (marker.isPopupOpen()) {
-                    //     marker.closePopup();
-                    // }
-                    controller.setSelectedMarkerData(uniqueCoords[uniqueCoordsKeys[i]]);
-                })
-
-                marker.on("mouseover", () => {
-                    // console.log("wat")
-                    marker.setStyle({fillColor: "#45CAFF"});
-                    let tooltipPos = marker.getLatLng();
-                    // tooltipPos.lat += (marker.getRadius() * 0.00001);
-                    // console.log(latlng.lat);                    
-                    // marker.openPopup(tooltipPos);
-                })
-
-                marker.on("mouseout", () => {
-                    // console.log("wat")
-                    marker.setStyle({fillColor: "#FF5710"});
-                    let tooltipPos = marker.getLatLng();
-                    // tooltipPos.lat += (marker.getRadius() * 0.00001);
-                    // console.log(latlng.lat);                    
-                    // marker.openPopup(tooltipPos);
-                })
-
-    
+                // While intersections still exist:
+                //   1. for each marker m1 that is not in a group:
+                //      1. find any markers m2 that are not in a group and m1 != m2, combine them into merged marker (add radius, find midpoint of i and marker)
+                //      2. mark intersecting markers as "merged" already (they should not be considered anymore)
+                //      3. don't mark i as merged (merged marker might end up intersecting with it in future)
+                // let i = 0;
+                let intersectionsStillExist = true; 
+                while (intersectionsStillExist) {
+                    intersectionsStillExist = false;
+                    // console.log(arrForCheckingIntersections);
+                    for (let i of arrForCheckingIntersections) {
+                        if (i.inGroup) {
+                            continue;
+                        }
+                        // let mergedMarker = JSON.parse(JSON.stringify(i));
+                        // mergedMarker.merged = false;
+                        // i.merged = true;
+                        for (let j of arrForCheckingIntersections) {
+                            // if they are the same or already in the same group, don't compare it
+                            if (i.id === j.id || j.inGroup) {
+                                continue;
+                            }
+                            // logic for computing intersection
+                            const distance = Math.sqrt(Math.pow(i.coords.x - j.coords.x, 2) + Math.pow(i.coords.y - j.coords.y, 2));
+                            let r1 = i.radius;
+                            let r2 = j.radius;
+                            if (distance <= r1 - r2 || distance <= r2 - r1 || distance < r1 + r2 || distance == r1 + r2) {
+                                intersectionsStillExist = true;
+                                // decide rules for the new marker groups size and position
+                                if (i.radius > j.radius) {
+                                    i.members.push(j.id);
+                                    // i.members = i.members.concat(j.members);
+                                    i.people = i.people.concat(j.people);
+                                    // j.members = [];
+                                    j.inGroup = true;
+                                    i.radius += 1;
+                                } else {
+                                    j.members.push(i.id);
+                                    // i.members = i.members.concat(j.members);
+                                    j.people = j.people.concat(i.people);
+                                    // j.members = [];
+                                    i.inGroup = true;
+                                    j.radius += 1;
+                                }
+                                // i.radius = i.radius + 1;
+                                // const midpoint = {
+                                //     x: (i.coords.x + j.coords.x) / 2,
+                                //     y: (i.coords.y + j.coords.y) / 2
+                                // }
+                                // i.coords.x = midpoint.x;
+                                // i.coords.y = midpoint.y;
+                                
                 
-            }
-            // check for any intersections (could be optimized?)
-            for (let i of arrForCheckingIntersections) {
-                // console.log(i)
-                for (let j of arrForCheckingIntersections) {
-                    if (i === j) { // marker always intersects with itself
-                        continue;
+                                
+                            }  
+                        }
                     }
-                    const distance = Math.sqrt(Math.pow(i.coords.x - j.coords.x, 2) + Math.pow(i.coords.y - j.coords.y, 2));
-                    // const midpoint = [i.coords.x - j.coords.x, ]
-                    // console.log(distance);
-                    const r1 = i.radius;
-                    const r2 = j.radius;
-                    if (distance <= r1 - r2) {
-                        i.markerRef.setStyle({fillColor: "black"});
-                        j.markerRef.setStyle({fillColor: "black"});
-                        // console.log("Circle B is inside A");
-                        // let marker = new L.circleMarker(
-                        //     [uniqueCoords[uniqueCoordsKeys[i]].lat, uniqueCoords[uniqueCoordsKeys[i]].lng],
-                        //     {
-                        //         color: "blue",
-                        //         radius: r1 + r2,
-                        //         stroke: false,
-                        //         fillOpacity: 0.5
-                        //     }
-                        // )
-                    }
-                    else if (distance <= r2 - r1) {
-                        i.markerRef.setStyle({fillColor: "black"});
-                        j.markerRef.setStyle({fillColor: "black"});
-                        // console.log("Circle A is inside B");
-                        // let marker = new L.circleMarker(
-                        //     [uniqueCoords[uniqueCoordsKeys[i]].lat, uniqueCoords[uniqueCoordsKeys[i]].lng],
-                        //     {
-                        //         color: "#FF5710",
-                        //         radius: uniqueCoords[uniqueCoordsKeys[i]].circleRadius + mapManager.map.getZoom(),
-                        //         stroke: false,
-                        //         fillOpacity: 0.5
-                        //     }
-                        // )
-                    }
-                    else if (distance < r1 + r2) {
-                        i.markerRef.setStyle({fillColor: "black"});
-                        j.markerRef.setStyle({fillColor: "black"});
-                        // console.log("Circle intersect to each other");
-                        // let marker = new L.circleMarker(
-                        //     [uniqueCoords[uniqueCoordsKeys[i]].lat, uniqueCoords[uniqueCoordsKeys[i]].lng],
-                        //     {
-                        //         color: "#FF5710",
-                        //         radius: uniqueCoords[uniqueCoordsKeys[i]].circleRadius + mapManager.map.getZoom(),
-                        //         stroke: false,
-                        //         fillOpacity: 0.5
-                        //     }
-                        // )
-                    }
-                    else if (distance == r1 + r2) {
-                        // console.log("Circle touch to each other");
-                    }
-                    else {
-                        // console.log("Circle not touch to each other");
-                    }
-                    // if () {
-                        
-                    // }
+                }
+                // all the markers that are marked inGroup are already part of a group
+                arrForCheckingIntersections = arrForCheckingIntersections.filter(markerGroup => !markerGroup.inGroup);
+                console.log(arrForCheckingIntersections)
+                for (let i of arrForCheckingIntersections) {
+                    let marker = new L.circleMarker(
+                        mapManager.map.containerPointToLatLng(i.coords),
+                        {
+                            color: "#FF5710",
+                            radius: i.radius,
+                            stroke: false,
+                            fillOpacity: 0.5
+                        }
+                    )
+                    marker.on("click", (e) => {
+                        console.log("click")
+                        controller.setSelectedMarkerData(i);
+                    })
+                    // i.markerRef = marker;
+                    marker.on("mouseover", () => {
+                        // console.log("wat")
+                        marker.setStyle({fillColor: "#45CAFF"});
+                        // let tooltipPos = marker.getLatLng();
+                        // tooltipPos.lat += (marker.getRadius() * 0.00001);
+                        // console.log(latlng.lat);                    
+                        // marker.openPopup(tooltipPos);
+                    })
+
+                    marker.on("mouseout", () => {
+                        // console.log("wat")
+                        marker.setStyle({fillColor: "#FF5710"});
+                        // let tooltipPos = marker.getLatLng();
+                        // tooltipPos.lat += (marker.getRadius() * 0.00001);
+                        // console.log(latlng.lat);                    
+                        // marker.openPopup(tooltipPos);
+                    })
+                    this.markerDOMEles.push(marker);
+                    marker.addTo(mapManager.map);
+                }
+
+            } else {
+                for (let i = 0; i < uniqueCoordsKeys.length; i++) {
+                    // let popupContent = `${uniqueCoords[uniqueCoordsKeys[i]].people.length}`;
+                    
+                    // console.log(mapManager.map.latLngToContainerPoint([uniqueCoords[uniqueCoordsKeys[i]].lat, uniqueCoords[uniqueCoordsKeys[i]].lng]));
+                    let marker = new L.circleMarker(
+                        [data[uniqueCoordsKeys[i]].lat, data[uniqueCoordsKeys[i]].lng],
+                        {
+                            color: "#FF5710",
+                            radius: data[uniqueCoordsKeys[i]].people.length + mapManager.map.getZoom(),
+                            stroke: false,
+                            fillOpacity: 0.5
+                        }
+                    )
+                    this.markerDOMEles.push(marker);
+
+                    
+                    // marker.on("add", () => {
+                    //     console.log("added to map")
+                    // })
+
+                    marker.on("click", (e) => {
+                        // if (marker.isPopupOpen()) {
+                        //     marker.closePopup();
+                        // }
+                        controller.setSelectedMarkerData(data[uniqueCoordsKeys[i]]);
+                    })
+
+                    marker.on("mouseover", () => {
+                        // console.log("wat")
+                        marker.setStyle({fillColor: "#45CAFF"});
+                        let tooltipPos = marker.getLatLng();
+                        // tooltipPos.lat += (marker.getRadius() * 0.00001);
+                        // console.log(latlng.lat);                    
+                        // marker.openPopup(tooltipPos);
+                    })
+
+                    marker.on("mouseout", () => {
+                        // console.log("wat")
+                        marker.setStyle({fillColor: "#FF5710"});
+                        let tooltipPos = marker.getLatLng();
+                        // tooltipPos.lat += (marker.getRadius() * 0.00001);
+                        // console.log(latlng.lat);                    
+                        // marker.openPopup(tooltipPos);
+                    })
+
+                    marker.addTo(mapManager.map);
                 }
             }
         }
@@ -241,12 +285,21 @@
                 document.querySelector("#designer-description #about-blurb-1").innerHTML = data.people[index].aboutBlurb1;
                 document.querySelector("#designer-description #about-blurb-2").innerHTML = data.people[index].aboutBlurb2;
             })
+            const radioButtons = document.querySelectorAll('input[name="marker-view-mode"]');
+            for (const radioButton of radioButtons) {
+                radioButton.addEventListener("click", () => {
+                    if (radioButton.checked) {
+                        controller.setMarkerViewMode(radioButton.value);
+                    }
+                });
+            }
+            
             this.render();
         },
 
         render() {
             const data = controller.getSelectedMarkerData();
-            console.log(data);
+            // console.log(data);
             controlPanelView.dropdownDOMEle.innerHTML = "";
             if (!data) {
                 controlPanelView.dropdownDOMEle.innerHTML = `<option value="" disabled selected>No location selected</option>`;
@@ -275,15 +328,15 @@
         async loadAndProcessDataset() {
             const data = await d3.csv("./CoPED advisory list 2021-1.csv", d3.autoType);
             data.sort((a,b) => b["Number"] - a["Number"]);
+
+            // group data based on latitude and longitude
             let uniqueCoords = {};
             data.forEach(datum => {
-                // console.log(datum)
                 const latlnString = `${datum["Latitude"]}, ${datum["Longitude"]}`;
                 if (!(latlnString in uniqueCoords)) {
                     uniqueCoords[latlnString] = {
                         lat: datum["Latitude"],
                         lng: datum["Longitude"],
-                        circleRadius: 0,
                         people: []
                     };
                 }
@@ -297,7 +350,6 @@
                     aboutBlurb1: datum["notes"],
                     aboutBlurb2: datum["Possible Speakers"]
                 })
-                uniqueCoords[latlnString].circleRadius++;
                 
             })
             return uniqueCoords;
@@ -318,6 +370,15 @@
 
         getSelectedMarkerData() {
             return model.selectedMarkerData;
+        },
+
+        setMarkerViewMode(mode) {
+            model.markerViewMode = mode;
+            markerManager.renderMarkers();
+        },
+
+        getMarkerViewMode() {
+            return model.markerViewMode;
         }
     }
     
